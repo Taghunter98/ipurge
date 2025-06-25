0220d4f9631780622f00fb26047ee280aaf1498c
= <=>
2025-06-25 09:10:28 +0100

    Added Config struct, added CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,47 @@
 //! ## Examples and Usage
 //!
 //! Create a .env file with the endpoint and folder destination for purging.
-//! 
+//!
 //! ```plaintext
 //! URL='https://myendpoint'
 //! DIR_PATH='/home/files'
 //! ```
-//! 
-//! Then build and run the apllication, ipurge will then run
+//!
+//! Then build and run the apllication passing the cronjob time as the argument.
+//!
+//! ## Examples
+//! Run the cronjob once a day at midnight
+//! ```bash
+//! cargo run '0 0 * * * *' 'https://website.com/endpoint' '/home/user/purgedirectoty'
+//! ```
 //!
 //! ## License
 //!
 //! Copyright (C) Josh Bassett, www.whondo.com. All rights reserved.
 //!
 
-use dotenv::dotenv;
 use ftail::Ftail;
 use log::LevelFilter;
+use std::env;
 use std::path::Path;
+
 pub mod purge;
+
+pub struct Config {
+    runtime: String,
+    endpoint: String,
+    path: String,
+}
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config {
+        runtime: args[1].trim().to_string(),
+        endpoint: args[2].trim().to_string(),
+        path: args[3].trim().to_string(),
+    };
 
     let path: &Path = Path::new("./logs/purge.txt");
 
@@ -35,5 +54,5 @@ async fn main() {
         .init()
         .expect("unable to create logger");
 
-    purge::run("0/5 * * * * *").await; // add this as args
+    purge::run(config).await;
 }
--- src/purge.rs
+++ src/purge.rs
@@ -1,18 +1,51 @@
+//! This module provides an api to run a conjob to delete unused user directories.
+//!
+//! ## License
+//!
+//! Copyright (C) Josh Bassett, www.whondo.com. All rights reserved.
+//!
+//! Apache 2.0
+//!
+
 use chrono::{Local, Utc};
 use cron::Schedule;
 use reqwest;
 use serde_json::{self, Value};
-use std::env::var;
 use std::fs;
 use std::str::FromStr;
 use std::thread;
 
-pub async fn run(runtime: &'static str) {;
+use crate::Config;
+
+/// Runs a cronjob that works through a list of directories and compares with email records from the database.
+///
+/// Function creates a new schedule and starts a continous loop running the cronjob as per user input.
+/// 
+/// The job runs the function `purge_heritics` that removes all unused directories and logs the purged emails.
+///
+/// # Panics
+///
+/// - If the `config.runtime` conversion fails.
+/// - If the `Schedule` fails.
+///
+/// # Examples
+/// ```rust
+/// let config = Config {
+///     runtime: "0 0 * * * *",
+///     endpoint: "https://website.com/endpoint",
+///     path: "/home/user/purgedirectory"
+/// }
+/// 
+/// run(config)
+/// ```
+///
+pub async fn run(config: Config) {
     let schedule: Schedule =
-        Schedule::from_str(runtime).expect("Failed to parse CRON expression");
+        Schedule::from_str(config.runtime.as_str()).expect("Failed to parse CRON expression");
 
     loop {
         let now: chrono::DateTime<Utc> = Utc::now();
+
         if let Some(next) = schedule.upcoming(Utc).take(1).next() {
             let until_next = next - now;
             thread::sleep(until_next.to_std().unwrap());
@@ -21,25 +54,25 @@ pub async fn run(runtime: &'static str) {;
                 Local::now().format("%Y-%m-%d %H:%M:%S")
             );
 
-            purge_heritics().await;
-
-            log::info!(
-                "Completed purge: {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S")
-            );
+            purge_heritics(&config).await;
         }
     }
 }
 
-async fn purge_heritics() {
-    let path: String = var("DIR_PATH").expect("unable to get value");
 
-    println!("{:?}", path.as_str());
-
-    let purge_list: Vec<String> = validate_directories(&path.as_str()).await;
+/// Purges unused directories 'heritics' from the given path.
+/// 
+/// # Panics
+///
+/// - If the `Future` is not cxecuted successfully.
+/// - If the `config.path` conversion fails.
+/// - If the directory is not removable.
+///
+async fn purge_heritics(config: &Config) {
+    let purge_list: Vec<String> = validate_directories(&config).await;
 
     for heritic in purge_list {
-        let heritic_path: String = format!("{}/{}", path.as_str(), heritic);
+        let heritic_path: String = format!("{}/{}", config.path.as_str(), heritic);
 
         match fs::remove_dir(heritic_path) {
             Ok(_) => log::info!("REM: {}", heritic),
@@ -48,17 +81,28 @@ async fn purge_heritics() {
     }
 }
 
-async fn validate_directories(path: &str) -> Vec<String> {
-    let emails: Vec<serde_json::Value> = parse_result().await;
+/// Validates directories and returns a purge list.
+/// 
+/// Function reviews all subdirectories of the `config.path` and compares with the email list.
+/// 
+/// The unused directories are added to a purge list to be remvoed.
+/// 
+/// # Panics
+///
+/// - If the `Future` is not executed succssfully.
+/// - If the directory read fails.
+///
+async fn validate_directories(config: &Config) -> Vec<String> {
+    let emails: Vec<Value> = parse_result(&config).await;
     let mut purge_list: Vec<String> = Vec::new();
 
-    for entry in fs::read_dir(path).expect("directory read failed") {
+    for entry in fs::read_dir(&config.path).expect("directory read failed") {
         let name: String = match entry.expect("entry name failed").file_name().into_string() {
             Ok(s) => s,
             Err(_) => continue,
         };
 
-        if !emails.iter().any(|e| e[0].as_str() == Some(&name)) {
+        if !emails.iter().any(|e: &Value| e[0].as_str() == Some(&name)) {
             purge_list.push(name);
         }
     }
@@ -66,20 +110,34 @@ async fn validate_directories(path: &str) -> Vec<String> {
     purge_list
 }
 
-async fn parse_result() -> Vec<Value> {
-    let result: String = get_emails().await.expect("unable to get result");
+/// Parses request into a `Vector<Value>` of emails.
+/// 
+/// # Panics
+/// 
+/// - If the `Future` is not cxecuted successfully.
+/// - If the `Value` is not parsable.
+/// - If the `Value` is not converted to a `Vec<Value>`.
+///
+async fn parse_result(config: &Config) -> Vec<Value> {
+    let result: String = get_emails(&config).await.expect("unable to get result");
     let json: Value = serde_json::from_str(result.as_str()).expect("unable to parse result");
 
     json["emails"].as_array().unwrap().to_vec()
 }
 
-async fn get_emails() -> Result<String, Box<dyn std::error::Error>> {
-    let url: String = std::env::var("URL").expect("unable to get value");
-
-    let response = reqwest::get(url).await?;
+/// Makes a GET request to weatherapi.com.
+/// 
+/// # Errors
+///
+/// - If the GET request fails.
+/// - If the response text fails.
+/// - If the HTTP `response.status()` is not 200 OK.
+///
+async fn get_emails(config: &Config) -> Result<String, Box<dyn std::error::Error>> {
+    let response: reqwest::Response = reqwest::get(config.endpoint.as_str()).await?;
 
     if response.status().is_success() {
-        let text = response.text().await?;
+        let text: String = response.text().await?;
         Ok(text)
     } else {
         Err(format!("HTTP response: {}", response.status()).into())
